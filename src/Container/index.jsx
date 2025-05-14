6e6a2e96f4a655c5e8f1e7d8422911a66b2a32d7
arthurgranito <arthurcursotecnico@gmail.com>
2025-05-14 09:13:12 -0300

    translation to english

--- src/Container/index.jsx
+++ src/Container/index.jsx
@@ -2,49 +2,49 @@ import { useState } from "react";
 
 const Container = () => {
     const apiKey = '37548af118b08e733b0c1e4a235a095c';
-    const [busca, setBusca] = useState('');
-    const [cidade, setCidade] = useState('');
-    const [pais, setPais] = useState('');
-    const [icone, setIcone] = useState('');
-    const [temperatura, setTemperatura] = useState('');
-    const [descricao, setDescricao] = useState('');
-    const [temperaturaMax, setTemperaturaMax] = useState('');
-    const [temperaturaMin, setTemperaturaMin] = useState('');
-    const [umidade, setUmidade] = useState('');
-    const [vento, setVento] = useState('');
-    const [alerta, setAlerta] = useState('');
-    const [existe, setExiste] = useState(undefined);
-    const [carregando, setCarregando] = useState(false)
+    const [search, setSearch] = useState('');
+    const [city, setCity] = useState('');
+    const [country, setCountry] = useState('');
+    const [icon, setIcon] = useState('');
+    const [temperature, setTemperature] = useState('');
+    const [description, setDescription] = useState('');
+    const [maxTemperature, setMaxTemperature] = useState('');
+    const [minTemperature, setMinTemperature] = useState('');
+    const [humidity, setHumidity] = useState('');
+    const [wind, setWind] = useState('');
+    const [alert, setAlert] = useState('');
+    const [exists, setExists] = useState(undefined);
+    const [loading, setLoading] = useState(false)
 
-    const pegarCidade = () => {
-        if (busca == '') {
-            setExiste(false);
-            setAlerta('Digite uma cidade')
+    const getCity = () => {
+        if (search === '') {
+            setExists(false);
+            setAlert('Enter a city')
         } else {
-            setCarregando(true)
-            setExiste(undefined)
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(busca)}&appid=${apiKey}&units=metric&lang=pt_br`)
+            setLoading(true)
+            setExists(undefined)
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(search)}&appid=${apiKey}&units=metric&lang=en`)
                 .then(response => response.json())
-                .then(infosCidade => {
-                    if (infosCidade.name == undefined) {
-                        setExiste(false)
-                        setAlerta('Cidade não encontrada')
+                .then(cityInfo => {
+                    if (cityInfo.name === undefined) {
+                        setExists(false)
+                        setAlert('City not found')
                     } else {
-                        setExiste(true)
-                        console.log(infosCidade)
-                        setCidade(infosCidade.name);
-                        setPais(infosCidade.sys.country)
-                        setIcone(infosCidade.weather[0].icon)
-                        setTemperatura(((infosCidade.main.temp).toString()).replace('.', ','))
-                        setDescricao(infosCidade.weather[0].description)
-                        setTemperaturaMax(((infosCidade.main.temp_max).toString()).replace('.', ','))
-                        setTemperaturaMin(((infosCidade.main.temp_min).toString()).replace('.', ','))
-                        setUmidade(((infosCidade.main.humidity).toString()).replace('.', ','))
-                        setVento(infosCidade.wind.speed)
+                        setExists(true)
+                        console.log(cityInfo)
+                        setCity(cityInfo.name);
+                        setCountry(cityInfo.sys.country)
+                        setIcon(cityInfo.weather[0].icon)
+                        setTemperature(((cityInfo.main.temp).toString()).replace('.', ','))
+                        setDescription(cityInfo.weather[0].description)
+                        setMaxTemperature(((cityInfo.main.temp_max).toString()).replace('.', ','))
+                        setMinTemperature(((cityInfo.main.temp_min).toString()).replace('.', ','))
+                        setHumidity(((cityInfo.main.humidity).toString()).replace('.', ','))
+                        setWind(cityInfo.wind.speed)
                     }
                 })
                 .finally(() => {
-                    setCarregando(false)
+                    setLoading(false)
                 })
         }
     }
@@ -54,75 +54,75 @@ const Container = () => {
             <div className="container">
                 <form onSubmit={(e) => {
                     e.preventDefault();
-                    pegarCidade();
-                    setBusca('');
+                    getCity();
+                    setSearch('');
                 }}>
                     <i className="fa-solid fa-location-dot"></i>
-                    <input type="text" value={busca} onChange={alterado => setBusca(alterado.target.value)} placeholder="Digite sua cidade..." />
+                    <input type="text" value={search} onChange={changed => setSearch(changed.target.value)} placeholder="Enter your city..." />
                     <button type="submit">
                         <i className="fa-solid fa-magnifying-glass"></i>
                     </button>
                 </form>
 
                 {
-                    carregando && <div className="carregando">
-                    <div className="bolinha"></div>
+                    loading && <div className="loading">
+                        <div className="spinner"></div>
                     </div>
                 }
 
-                <h3 className={existe == false ? "alerta ativo" : "alerta"}>{alerta} <i className="fa-solid fa-face-sad-tear"></i></h3>
+                <h3 className={exists === false ? "alert active" : "alert"}>{alert} <i className="fa-solid fa-face-sad-tear"></i></h3>
 
-                <div className={existe == true ? "info active" : "info"}>
-                    <h1 className="cidade">{`${cidade}, ${pais}`}</h1>
-                    <div className="div_temp">
-                        <img src={`https://openweathermap.org/img/wn/${icone}.png`} alt="Ícone do tempo" className="icone" />
-                        <div className="textos">
-                            <h1 className="temperatura">{temperatura} <sup>C°</sup></h1>
-                            <p className="descricao">{descricao}</p>
+                <div className={exists === true ? "info active" : "info"}>
+                    <h1 className="city">{`${city}, ${country}`}</h1>
+                    <div className="temp_div">
+                        <img src={`https://openweathermap.org/img/wn/${icon}.png`} alt="Weather Icon" className="icon" />
+                        <div className="texts">
+                            <h1 className="temperature">{temperature} <sup>C°</sup></h1>
+                            <p className="description">{description}</p>
                         </div>
                     </div>
 
-                    <div className="outrasInfos">
-                        <div className="outraInfo">
+                    <div className="otherInfos">
+                        <div className="otherInfo">
                             <i className="fa-solid fa-temperature-high" id="temp_max_icon"></i>
 
                             <div>
-                                <h2>Temp. Máx</h2>
+                                <h2>Max Temp</h2>
                                 <p>
-                                    {temperaturaMax} <sup>C°</sup>
+                                    {maxTemperature} <sup>C°</sup>
                                 </p>
                             </div>
                         </div>
 
-                        <div className="outraInfo">
+                        <div className="otherInfo">
                             <i className="fa-solid fa-temperature-low" id="temp_min_icon"></i>
 
                             <div>
-                                <h2>Temp. Mín</h2>
+                                <h2>Min Temp</h2>
                                 <p id="temp_min">
-                                    {temperaturaMin} <sup>C°</sup>
+                                    {minTemperature} <sup>C°</sup>
                                 </p>
                             </div>
                         </div>
 
-                        <div className="outraInfo">
-                            <i className="fa-solid fa-droplet" id="temp_umidity_icon"></i>
+                        <div className="otherInfo">
+                            <i className="fa-solid fa-droplet" id="temp_humidity_icon"></i>
 
                             <div>
-                                <h2>Umidade</h2>
-                                <p id="temp_umidity">
-                                    {umidade}%
+                                <h2>Humidity</h2>
+                                <p id="temp_humidity">
+                                    {humidity}%
                                 </p>
                             </div>
                         </div>
 
-                        <div className="outraInfo">
+                        <div className="otherInfo">
                             <i className="fa-solid fa-wind" id="temp_wind_icon"></i>
 
                             <div>
-                                <h2>Vento</h2>
+                                <h2>Wind</h2>
                                 <p id="temp_wind">
-                                    {vento} km/h
+                                    {wind} km/h
                                 </p>
                             </div>
                         </div>
